2ad3dc152c93b38fbb571bf9696fdd2f4d62fc31
Jakub Bieńkowski <jbienkowski@fandom.com>
2024-04-09 13:33:09 +0200

    AoC 2020 Day 1 Part 1&2

--- 2020/1/index.js
+++ 2020/1/index.js
@@ -10,3 +10,44 @@ const input = fs
 
 console.log('Input file:', inputFile);
 console.log('Input', input);
+
+const sorted = input.map(Number).sort((a,b) => a - b);
+console.log('Result - part 1:', partOne(sorted));
+console.log('Result - part 2:', partTwo(sorted));
+
+function twoPointerSearch(input, target) { 
+  let left = 0;
+  let right = input.length -1;
+
+  while(input[left] + input[right] !== target) { 
+    // sum is less than the target, increase left pointer
+    if(input[left] + input[right] < target){
+      left++;
+    // sum is greater than the target, decrease right pointer
+    } else {
+      right--;
+    }
+  }
+
+  if (input[left] + input[right] === target) {
+    return [input[left], input[right]]
+  }
+
+  return [0, 0];
+}
+
+function partOne(sorted) {
+  const [a, b] = twoPointerSearch(sorted, 2020);
+  return a * b;
+}
+
+function partTwo(sorted) {
+  const target = 2020;
+  for (let i = 0; i < sorted.length; i++) {
+    const [a, b] = twoPointerSearch(sorted, target - sorted[i]);
+    if (sorted[i] + a + b === target) {
+      return sorted[i] * a * b;
+    }
+  }
+  return 0;
+}
