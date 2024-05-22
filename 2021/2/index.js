f60bc39dfed8795089ceb5d56a4fd9007297f825
Jakub Bieńkowski <jbienkowski@fandom.com>
2024-05-22 13:00:43 +0200

    2021 day 2

--- 2021/2/index.js
+++ 2021/2/index.js
@@ -10,3 +10,56 @@ const input = fs
 
 console.log('Input file:', inputFile);
 console.log('Input', input);
+
+function partOne() {
+  let position = 0, depth = 0;
+
+  processCommands(
+    (units) => position += units,
+    (units) => depth -= units,
+    (units) => depth += units
+  );
+
+  return [position, depth];
+}
+
+function partTwo() {
+  let position = 0, depth = 0, aim = 0;
+
+  processCommands(
+    (units) => {
+      position += units;
+      depth += aim * units;
+    },
+    (units) => aim -= units,
+    (units) => aim += units
+  );
+
+  return [position, depth, aim];
+}
+
+function processCommands(onForward, onUp, onDown) {
+  for (const line of input) {
+    const [command, units] = line.split(' ');
+    const u = Number(units);
+    switch (command) {
+      case 'forward':
+        onForward(u);
+        break;
+      case 'up':
+        onUp(u);
+        break;
+      case 'down':
+        onDown(u);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+const [p1, d1] = partOne();
+const [p2, d2, a2] = partTwo();
+
+console.log('Part 1', p1, d1, 'Result:', p1 * d1);
+console.log('Part 2', p2, d2, a2, 'Result:', p2 * d2);
