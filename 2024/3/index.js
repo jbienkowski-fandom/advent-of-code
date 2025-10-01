c7354ceddb72306bad161efbd7e91c1959fce6b3
Jakub Bieńkowski <jbienkowski@fandom.com>
2025-10-01 11:58:34 +0200

    AoC 2024 day 3

--- 2024/3/index.js
+++ 2024/3/index.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
-const testFile = process.argv[3] || 'input.txt';
+const testFile = process.argv[2] || 'input.txt';
 
 let input = fs
     .readFileSync(testFile, 'utf8')
     .split(/\r\n/)
     .map((line) => line.split('\n'))[0]
-    .filter((line) => !!line)[0]
-    .split(' ')
-    .map(Number);
+    .filter((line) => !!line);
 
-console.log('[Advent of Code 2024 - Day 3]', testFile, input);
+console.log('[Advent of Code 2024 - Day 3]', testFile, JSON.stringify(input, null, 2));
